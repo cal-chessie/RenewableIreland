d0d8b9f149b4a631e1d772ac75bc43b5febe2fb6
Z User <z@container>
2026-04-10 20:47:27 +0000

    1225022d-2b4b-4c7a-a0d8-f5e42a758bf6

--- keepalive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while true; do
-  cd /home/z/my-project
-  node --max-old-space-size=4096 node_modules/.bin/next dev -p 3000 >> /home/z/my-project/dev.log 2>&1
-  echo "Server died at $(date). Restarting in 3s..." >> /home/z/my-project/dev.log
-  sleep 3
-done
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-exec node --max-old-space-size=4096 node_modules/.bin/next dev -p 3000
